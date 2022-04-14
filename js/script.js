14bd34d6a5d95515ecf14e82a67d36a92ba47756
gyxii <gyeongas383@gmail.com>
2022-04-15 01:15:17 +0900

    css 수정

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const content = "Web Publisher."
+const text = document.querySelector(".text")
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(3000);
+    }
+}
+setInterval(typing, 200)
+
+
