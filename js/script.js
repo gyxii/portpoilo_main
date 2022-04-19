7b84d7b2ea1bbd48be8e6e7c3457ba52350218e6
gyxii <gyeongas383@gmail.com>
2022-04-19 20:48:52 +0900

    반응형 수정

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const content = "Web Publisher."
 const text = document.querySelector(".text")
 let index = 0;
 
- function sleep(delay){ 
-     const start = new Date().getTime(); 
-     while (new Date().getTime() < start + delay); 
-    }
+function sleep(delay) {
+  const start = new Date().getTime();
+  while (new Date().getTime() < start + delay);
+}
 
-function typing(){
-    text.textContent += content[index++];
-    if(index > content.length){
-        text.textContent = ""
-        index = 0;
-        sleep(3000);
-    }
+function typing() {
+  text.textContent += content[index++];
+  if (index > content.length) {
+    text.textContent = ""
+    index = 0;
+    sleep(3000);
+  }
 }
 setInterval(typing, 200)
 
@@ -25,10 +25,10 @@ setInterval(typing, 200)
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home_contact');
 homeContactBtn.addEventListener('click', () => {
- 
-        const scrollTo = document.querySelector('#contact');
-        scrollTo.scrollIntoView({ behavior: 'smooth' });
-     
+
+  const scrollTo = document.querySelector('#contact');
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+
 });
 
 
